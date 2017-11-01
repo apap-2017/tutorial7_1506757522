14097114056c2adb7777f6a520958b0c1d3cfe72
DESKTOP-8H3JI9I\Hp <joachimichwan@gmail.com>
2017-11-01 12:49:28 +0700

    latihan 1 dan 2

---
+++ Tutorial07Producer/src/main/java/com/example/APAPTutorial07ProducerApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class APAPTutorial07ProducerApplication
+{
+
+    public static void main (String[] args)
+    {
+        SpringApplication.run (APAPTutorial07ProducerApplication.class, args);
+    }
+}
